726799912cad45ba5d795ec0efdd3557e8984399
Chetan Dattaram Rane <chetan_rane@persistent.com>
2017-11-23 13:58:03 +0530

    COnfig for prod and local

--- src/app/config.js
+++ src/app/config.js
@@ -1,18 +1,22 @@
+const env = "dev";
+const url =  env == 'prod' ? 'http://rlldchdp01.pactiv.com/lvpilot/Data/' : "http://localhost/RankLousvillePOC/Data/";
+
 const Config = {
     title : 'Lousville Project POC',
     beta : true,
+
     restUrls : {
-        SUMMARY_DATA : "http://localhost/RankLousvillePOC/Data/getSummaryData.php",
-        REJECT_REASON_COUNT : "http://localhost/RankLousvillePOC/Data/getRejectReasonCount.php",
-        ML_IMPORTANT_FACTORS : "http://localhost/RankLousvillePOC/Data/getFactors.php",
-        ML_IMPORTANT_FACTORS_MILL_SPEED : "http://localhost/RankLousvillePOC/Data/getFactorsForMillSpeed.php",
-        ML_COIL_FACTORS : "http://localhost/RankLousvillePOC/Data/getMLForCoilFactors.php",
-        MILLS : "http://localhost/RankLousvillePOC/Data/getMills.php",
-        CONTROL_LIMITS : "http://localhost/RankLousvillePOC/Data/getControlLimits.php",
-        MILL_SPEED_TREND : "http://localhost/RankLousvillePOC/Data/getMillSpeedTrend.php",
-        FILTERS : "http://localhost/RankLousvillePOC/Data/getFilters.php",
-        IDENTIFIED_REASONS : "http://localhost/RankLousvillePOC/Data/getIdentifiedReasons.php",
-        FACTOR_REASONS : "http://localhost/RankLousvillePOC/Data/getRejectFactorReason.php",
+        SUMMARY_DATA : url + "getSummaryData.php",
+        REJECT_REASON_COUNT : url + "getRejectReasonCount.php",
+        ML_IMPORTANT_FACTORS : url + "getFactors.php",
+        ML_IMPORTANT_FACTORS_MILL_SPEED : url + "getFactorsForMillSpeed.php",
+        ML_COIL_FACTORS : url + "getMLForCoilFactors.php",
+        MILLS : url + "getMills.php",
+        CONTROL_LIMITS : url + "getControlLimits.php",
+        MILL_SPEED_TREND : url + "getMillSpeedTrend.php",
+        FILTERS : url + "getFilters.php",
+        IDENTIFIED_REASONS : url + "getIdentifiedReasons.php",
+        FACTOR_REASONS : url + "getRejectFactorReason.php",
     },
     colors : {
         'Accepted' : '#cd6a40',
