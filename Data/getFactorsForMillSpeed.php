582e3d9b36711347ace2d076e5895b36754813d9
Chetan Dattaram Rane <chetan_rane@persistent.com>
2018-01-10 19:59:33 +0530

    removed space from the currentmill key

--- Data/getFactorsForMillSpeed.php
+++ Data/getFactorsForMillSpeed.php
@@ -14,12 +14,12 @@
 
 */
 $mills = array(
-    array('key' => 'mill1', 'value' => 'Mill 1', 'targetGauge' => [0.045, 0.085, .125]),
-    array('key' => 'mill2', 'value' => 'Mill 2', 'targetGauge' => [0.145, 0.085, .125]),
-    array('key' => 'mill3', 'value' => 'Mill 3', 'targetGauge' => [0.245, 0.085, .125]),
-    array('key' => 'mill4', 'value' => 'Mill 4', 'targetGauge' => [0.345, 0.085, .125]),
-    array('key' => 'mill5', 'value' => 'Mill 5', 'targetGauge' => [0.445, 0.085, .125]),
-    array('key' => 'mill6', 'value' => 'Mill 6', 'targetGauge' => [0.545, 0.085, .125]),
+    array('key' => 'mill1', 'value' => 'Mill1', 'targetGauge' => [0.045, 0.085, .125]),
+    array('key' => 'mill2', 'value' => 'Mill2', 'targetGauge' => [0.145, 0.085, .125]),
+    array('key' => 'mill3', 'value' => 'Mill3', 'targetGauge' => [0.245, 0.085, .125]),
+    array('key' => 'mill4', 'value' => 'Mill4', 'targetGauge' => [0.345, 0.085, .125]),
+    array('key' => 'mill5', 'value' => 'Mill5', 'targetGauge' => [0.445, 0.085, .125]),
+    array('key' => 'mill6', 'value' => 'Mill6', 'targetGauge' => [0.545, 0.085, .125]),
 );
 $plants = array("hotsprings", "louisville");
 $finalArray = array();
